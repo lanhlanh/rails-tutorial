35ed526d95f7d9c81bbb9bc16190a7e16680b51b
lanhlanh <a1k9lanh@gmail.com>
2019-05-14 14:02:44 +0700

    config production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,9 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+set :stage, :production
+set :rails_env, :production
+set :deploy_to, "/home/deploy/apps/rails-tutorial"
+set :branch, :master
+server "18.191.179.96", user: "deploy", roles: %w(web app db)
