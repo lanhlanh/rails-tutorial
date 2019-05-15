50259ce4a644e5eb3f2f509ca29616469816367d
lanhlanh <a1k9lanh@gmail.com>
2019-05-15 09:33:57 +0700

    set linked_file for database yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ server "18.222.233.192", roles: [:web, :app, :db], primary: true
 
 set :user,            "deploy"
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    2
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -29,13 +29,13 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 ## Defaults:
 # set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+set :branch,        :master
+set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
